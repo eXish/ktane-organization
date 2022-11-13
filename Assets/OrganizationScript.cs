0a63e1e56d126409a472359e55ce4537c36e5bab
TheDarkSid3r <33441906+TheDarkSid3r@users.noreply.github.com>
2022-11-13 10:48:27 -0800

    Fix TTKS fields for Custom Keys

--- Assets/OrganizationScript.cs
+++ Assets/OrganizationScript.cs
@@ -22,8 +22,8 @@ public class OrganizationScript : MonoBehaviour
         "Reordered Keys", "Misordered Keys", "Recorded Keys", "Disordered Keys", "Simon Sings", "Vectors", "Game of Life Cruel", "Mastermind Cruel", "Factory Maze", "Simon Sends", "Quintuples",
         "The Hypercube", "The Ultracube", "Lombax Cubes", "Bamboozling Button", "Simon Stores", "The Cube", "The Sphere", "Ten-Button Color Code", "LEGOs", "Unfair Cipher", "Ultimate Cycle", "Ultimate Cipher", "Bamboozled Again" };
 
-    private static readonly string[] ttksBefore = { "Morse Code", "Wires", "Two Bits", "The Button", "Colour Flash", "Round Keypad", "Password", "Who's On First", "Crazy Talk", "Keypad", "Listening", "Orientation Cube" };
-    private static readonly string[] ttksAfter = { "Semaphore", "Combination Lock", "Simon Says", "Astrology", "Switches", "Plumbing", "Maze", "Memory", "Complicated Wires", "Wire Sequence", "Cryptography" };
+    private string[] ttksBefore = { "Morse Code", "Wires", "Two Bits", "The Button", "Colour Flash", "Round Keypad", "Password", "Who's On First", "Crazy Talk", "Keypad", "Listening", "Orientation Cube" };
+    private string[] ttksAfter = { "Semaphore", "Combination Lock", "Simon Says", "Astrology", "Switches", "Plumbing", "Maze", "Memory", "Complicated Wires", "Wire Sequence", "Cryptography" };
     private List<string> order = new List<string>();
     private List<string> mysteryModuleHiddens = new List<string>();
     private List<string> mysteryModuleKeys = new List<string>();
